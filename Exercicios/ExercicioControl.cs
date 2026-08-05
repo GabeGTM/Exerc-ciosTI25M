088b2ad1db77bf7a032e6f5f0226b25c864e7e2b
GABRIEL TEIXEIRA MOURA DE ABREU <gabriel.tmabreu@senacsp.edu.br>
2026-08-05 11:37:32 -0300

    Primeiro Commit

---
+++ Exercicios/ExercicioControl.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioControl
+    {
+        ExercicioModel modelo;//Conectando a Classe Model e Control
+        private int opcao;
+
+        public ExercicioControl()
+        { 
+            this.modelo = new ExercicioModel();
+            SetOpcao(0);
+        }//Contrutor Padrão
+
+        public int GetOpcao()
+        { 
+            return this.opcao;
+        }//fim do getOpcao
+
+        public void SetOpcao(int opcao)
+        {
+            this.opcao = opcao;
+        }
+
+        public void Coletar()
+        {
+            //Peguei os dois
+            Console.Write("Informe o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Vou passar para o construtor com pârametro
+            this.modelo = new ExercicioModel(num1, num2);
+        }//fim do coletar = Pegar os dois números digitados
+
+        public void ColetarReal()
+        {
+            //Pegar dois números
+            Console.WriteLine("Informe o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            //Chamar o construtor que receba reais
+            this.modelo = new ExercicioModel(num1, num2
+                );
+        }//fim do ColetarReal
+
+        //Mostrar as opções de operaçã na tela
+        public void Menu()
+        {
+            Console.WriteLine("\nEscolha uma das opções abaixo: \n" +
+                              "0. Sair\n"                           +
+                              "1. Soma \n"                          +
+                              "2. Subtração \n"                     +
+                              "3. Divisão \n"                       +
+                              "4. Multiplicação\n"                  +
+                              "5. Potência\n"                       +
+                              "6. Potência com Parâmetros\n"        +
+                              "7. Raiz\n"                           +
+                              "8. Raiz com Parâmetros\n");
+            SetOpcao(Convert.ToInt32(Console.ReadLine()));
+            Console.Clear();//Limpa o console
+        }//fim
+
+        //Executar o menu
+        public void ExecutarMenu()
+        {
+            do
+            {
+                Menu();//Mostar as opções
+                switch (GetOpcao())
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        Coletar();//Pegar os dois números
+                        Console.WriteLine("A soma é: " + this.modelo.Somar());//Fazer a operação de soma
+                        break;//Finalizar o caso
+                    case 2:
+                        Coletar();
+                        this.modelo.Subtrair();
+                        Console.WriteLine("A subtração é:" + this.modelo.Subtrair());
+                        break;
+                    case 3:
+                        Coletar();
+                        //validação
+                        if (this.modelo.Dividir() <= -1)
+                        {
+                            Console.WriteLine("Impossível dividir por zero!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("A divisão é: " + this.modelo.Dividir());
+                        }
+                        break;
+                    case 4:
+                        Coletar();
+                        Console.WriteLine("A multiplicação é: " + this.modelo.Multiplicar());
+                        break;
+                    case 5:
+                        ColetarReal();
+                        Console.WriteLine("A potência é: " + this.modelo.Potencia());
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe a base: ");
+                        double bas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o expoente: ");
+                        double expoente = Convert.ToDouble(Console.ReadLine());
+                        //Mostrar o Resultado da operação
+                        Console.WriteLine("A potência é: " + this.modelo.Potencia(bas, expoente));
+                        break;
+                    case 7:
+                        ColetarReal();
+                        Console.WriteLine("A raiz do primeiro número é: " + this.modelo.RaizNum3());
+                        Console.WriteLine("A raiz do segundo número é: " + this.modelo.RaizNum4());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo número: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        //Chamar o método que calcula a raiz
+                        Console.WriteLine("A raiz do primeiro número é: " + this.modelo.Raiz(num1));
+                        Console.WriteLine("A raiz do segundo número é: " + this.modelo.Raiz(num2));
+                        break;
+                    default:
+                        Console.WriteLine("Opção escolhida não é valida!");
+                        break;
+                }//fim escolha
+            } while (GetOpcao() != 0);//fim do do...while
+        }//fim do método executar menu
+
+
+    }//fim da classe
+}//fim do projeto
